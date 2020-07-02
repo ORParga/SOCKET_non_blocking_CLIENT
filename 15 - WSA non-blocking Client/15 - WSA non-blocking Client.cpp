78cd956fad49921e74e760edfa64893ced4d218f
Oscar Rodriguez Parga <Oscar_rodriguez_parga@hotmail.com>
2020-07-02 18:26:47 +0200

    Add files via upload

---
+++ 15 - WSA non-blocking Client/15 - WSA non-blocking Client.cpp
@@ -0,0 +1,389 @@
+// 15 - WSA non-blocking Client.cpp : Define el punto de entrada de la aplicación.
+//
+
+#include "framework.h"
+#ifndef WM_SOCKET
+#define WM_SOCKET WM_USER + 1
+#endif // !WM_SOCKET
+#ifndef IDT_TIMER1
+#define IDT_TIMER1 1
+#endif // !IDT_TIMER1
+#include "15 - WSA non-blocking Client.h"
+
+#define MAX_LOADSTRING 100
+
+// Variables globales:
+HINSTANCE hInst;                                // instancia actual
+WCHAR szTitle[MAX_LOADSTRING];                  // Texto de la barra de título
+WCHAR szWindowClass[MAX_LOADSTRING];            // nombre de clase de la ventana principal
+//Manejadores de controles comunes
+HWND hwndButton, hwndEdit, hwndStatic, hwndStatic2, hwndMesageRec;
+RECT StaticRect1 = { 10,90,80,20 };
+RECT StaticRect2 = { 100,90,100,20 };
+RECT StaticRect3 = { 10,120,80,20 };
+RECT EditRect = { 100,120,0,0 };
+RECT ButtonRect = { 100,150,100,20 };
+
+//WSA_NON_BLOKING inicializacion y declaracion
+WSA_non_blocking_Client WSAnb_Client;
+
+wchar_t IPString[] = L"127.0.0.1";
+int portNumber = 27015;
+// Declaraciones de funciones
+ATOM                MyRegisterClass(HINSTANCE hInstance);
+HWND                InitInstance(HINSTANCE, int);
+LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+int Ini_WSA_non_blocking_client(HWND);
+void XTrace(LPCTSTR , ...);
+void XTrace0(LPCTSTR );
+void ShowIPandPORT(HDC , RECT* );
+int Ini_UI(HWND);
+void ShowMessageControls(BOOL );
+
+int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
+                     _In_opt_ HINSTANCE hPrevInstance,
+                     _In_ LPWSTR    lpCmdLine,
+                     _In_ int       nCmdShow)
+{
+    UNREFERENCED_PARAMETER(hPrevInstance);
+    UNREFERENCED_PARAMETER(lpCmdLine);
+
+    // TODO: Colocar código aquí.
+
+    // Inicializar cadenas globales
+    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+    LoadStringW(hInstance, IDC_MY15WSANONBLOCKINGCLIENT, szWindowClass, MAX_LOADSTRING);
+    MyRegisterClass(hInstance);
+
+    // Realizar la inicialización de la aplicación:
+    HWND hwnd = InitInstance (hInstance, nCmdShow);
+    if (!hwnd) return FALSE;
+
+    if (!Ini_WSA_non_blocking_client(hwnd))return FALSE;
+    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_MY15WSANONBLOCKINGCLIENT));
+
+    MSG msg;
+
+    // Bucle principal de mensajes:
+    while (GetMessage(&msg, nullptr, 0, 0))
+    {
+        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+        {
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
+
+    return (int) msg.wParam;
+}
+
+int Ini_WSA_non_blocking_client(HWND hwnd) {
+    if(WSAnb_Client.CreateClientSocket() == FALSE)return FALSE;
+    SetTimer(hwnd,             // handle to main window 
+        IDT_TIMER1,            // timer identifier 
+        2000,                 // 1-second interval 
+        (TIMERPROC)NULL);     // no timer callback 
+
+    if (!WSAnb_Client.bConnected)
+    {
+
+        //wchar_t IPString[] = L"192.168.137.1";
+        WSAnb_Client.Attemp_connect(IPString, portNumber);
+    }
+    return TRUE;
+}
+int Ini_UI(HWND hwnd) {
+    hwndStatic = CreateWindowEx(
+        0, L"STATIC",   // predefined class 
+        L"Recieve:",         // no window title 
+        WS_CHILD  |WS_VISIBLE| ES_LEFT,
+        StaticRect1.left,         // x position 
+        StaticRect1.top,         // y position 
+        StaticRect1.right,        // Button width
+        StaticRect1.bottom,        // Button height
+        hwnd,         // parent window 
+        NULL,           // No menu. 
+        hInst,
+        NULL);        // pointer not needed 
+
+    hwndMesageRec = CreateWindowEx(
+        0, L"STATIC",   // predefined class 
+        L"No Message",         // no window title 
+        WS_CHILD  | WS_BORDER | ES_LEFT,
+        StaticRect2.left,         // x position 
+        StaticRect2.top,         // y position 
+        StaticRect2.right,        // Button width
+        StaticRect2.bottom,        // Button height
+        hwnd,         // parent window 
+        NULL,           // No menu. 
+        hInst,
+        NULL);        // pointer not needed 
+
+    hwndStatic2 = CreateWindowEx(
+        0, L"STATIC",   // predefined class 
+        L"Send:",         // no window title 
+        WS_CHILD  | ES_LEFT,
+        StaticRect3.left,         // x position 
+        StaticRect3.top,         // y position 
+        StaticRect3.right,        // Button width
+        StaticRect3.bottom,        // Button height
+        hwnd,         // parent window 
+        NULL,           // No menu. 
+        hInst,
+        NULL);        // pointer not needed 
+
+    hwndButton = CreateWindow(
+        L"BUTTON",  // Predefined class; Unicode assumed 
+        L"SEND",      // Button text 
+        WS_TABSTOP  | WS_CHILD | BS_DEFPUSHBUTTON,  // Styles 
+        ButtonRect.left,         // x position 
+        ButtonRect.top,         // y position 
+        ButtonRect.right,        // Button width
+        ButtonRect.bottom,        // Button height
+        hwnd,     // Parent window
+        NULL,       // No menu.
+        hInst,
+        NULL);      // Pointer not needed.
+
+    hwndEdit = CreateWindowEx(
+        0, L"EDIT",   // predefined class 
+        NULL,         // no window title 
+        WS_CHILD |WS_BORDER| ES_LEFT ,
+        0, 0, 0, 0,   // set size in WM_SIZE message 
+        hwnd,         // parent window 
+        NULL,           // No menu. 
+        hInst,
+        NULL);        // pointer not needed 
+
+    return TRUE;
+}
+//
+//  FUNCIÓN: MyRegisterClass()
+//
+//  PROPÓSITO: Registra la clase de ventana.
+//
+ATOM MyRegisterClass(HINSTANCE hInstance)
+{
+    WNDCLASSEXW wcex;
+
+    wcex.cbSize = sizeof(WNDCLASSEX);
+
+    wcex.style          = CS_HREDRAW | CS_VREDRAW;
+    wcex.lpfnWndProc    = WndProc;
+    wcex.cbClsExtra     = 0;
+    wcex.cbWndExtra     = 0;
+    wcex.hInstance      = hInstance;
+    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_MY15WSANONBLOCKINGCLIENT));
+    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
+    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
+    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_MY15WSANONBLOCKINGCLIENT);
+    wcex.lpszClassName  = szWindowClass;
+    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+
+    return RegisterClassExW(&wcex);
+}
+
+//
+//   FUNCIÓN: InitInstance(HINSTANCE, int)
+//
+//   PROPÓSITO: Guarda el identificador de instancia y crea la ventana principal
+//
+//   COMENTARIOS:
+//
+//        En esta función, se guarda el identificador de instancia en una variable común y
+//        se crea y muestra la ventana principal del programa.
+//
+HWND InitInstance(HINSTANCE hInstance, int nCmdShow)
+{
+   hInst = hInstance; // Almacenar identificador de instancia en una variable global
+
+   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW,
+      CW_USEDEFAULT, 0, 300, 300, nullptr, nullptr, hInstance, nullptr);
+
+   if (!hWnd)
+   {
+      return 0;
+   }
+
+   ShowWindow(hWnd, nCmdShow);
+   UpdateWindow(hWnd);
+
+   return hWnd;
+}
+
+//
+//  FUNCIÓN: WndProc(HWND, UINT, WPARAM, LPARAM)
+//
+//  PROPÓSITO: Procesa mensajes de la ventana principal.
+//
+//  WM_COMMAND  - procesar el menú de aplicaciones
+//  WM_PAINT    - Pintar la ventana principal
+//  WM_DESTROY  - publicar un mensaje de salida y volver
+//
+//
+LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    switch (message)
+    {
+    case WM_TIMER:
+    {
+        switch (wParam)
+        {
+        case IDT_TIMER1:
+            XTrace(L"WM_TIMER\n");
+            switch (WSAnb_Client.state)
+            {
+            case WSA_non_blocking_Client::STATE::NONE:
+                //wchar_t IPString[] = L"192.168.137.1";
+                WSAnb_Client.Attemp_connect(IPString, portNumber);
+                break;
+            case WSA_non_blocking_Client::STATE::CONNECTED:
+                SetWindowText(hwndMesageRec, WSAnb_Client.IPString);
+                break;
+            default:
+                break;
+            }
+             WSAnb_Client.testForEvents();
+            InvalidateRect(hWnd, NULL, TRUE);
+            return 0;
+        default:
+            return DefWindowProc(hWnd, message, wParam, lParam);
+        }
+    }
+    case WM_CREATE :
+    {
+        Ini_UI(hWnd);
+        // Add text to the window. 
+        wchar_t text[] = L"Text for EditBox";
+        SendMessage(hwndEdit, WM_SETTEXT, 0, (LPARAM)text);
+        return 0;
+    }
+    case WM_COMMAND:
+        {
+        if (lParam== (LPARAM)hwndButton)
+        {
+            break;
+        }
+            int wmId = LOWORD(wParam);
+            // Analizar las selecciones de menú:
+            switch (wmId)
+            {
+            case IDM_ABOUT:
+                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+                break;
+            case IDM_EXIT:
+                DestroyWindow(hWnd);
+                break;
+            default:
+                return DefWindowProc(hWnd, message, wParam, lParam);
+            }
+        }
+        break;
+    case WM_SETFOCUS:
+        SetFocus(hwndEdit);
+        return 0;
+    case WM_PAINT:
+        {
+            PAINTSTRUCT ps;
+            HDC hdc = BeginPaint(hWnd, &ps);
+            // TODO: Agregar cualquier código de dibujo que use hDC aquí...
+            RECT TextRect = { 10,10,300,100 };
+            switch (WSAnb_Client.state)
+            {
+            case WSA_non_blocking_Client::STATE::NONE:
+                DrawText(hdc, L"Estado:None", -1, &TextRect, DT_VCENTER | DT_LEFT);
+                ShowMessageControls(FALSE);
+                break;
+            case WSA_non_blocking_Client::STATE::CONNECTED:
+                DrawText(hdc, L"Estado:Conectado", -1, &TextRect, DT_VCENTER | DT_LEFT);
+                ShowIPandPORT(hdc, &TextRect);
+                ShowMessageControls(TRUE);
+                break;
+            case WSA_non_blocking_Client::STATE::REQUESTING:
+                DrawText(hdc, L"Estado:Solicitando conexion", -1, &TextRect, DT_VCENTER | DT_LEFT);
+                ShowIPandPORT(hdc, &TextRect);
+                ShowMessageControls(FALSE);
+                break;
+            default:
+                DrawText(hdc, L"Estado:Desconocido", -1, &TextRect, DT_VCENTER | DT_LEFT);
+                ShowMessageControls(FALSE);
+                break;
+            }
+            EndPaint(hWnd, &ps);
+        }
+        break; 
+    case WM_SIZE:
+    {
+        // Make the edit control the size of the window's client area. 
+        MoveWindow(hwndMesageRec,
+            StaticRect2.left, StaticRect2.top,                    // starting x- and y-coordinates 
+            LOWORD(lParam)- StaticRect2.left-20,                // width of client area 
+            20,                // height of client area 
+            TRUE);                 // repaint window 
+
+        MoveWindow(hwndEdit,
+            EditRect.left, EditRect.top,                    // starting x- and y-coordinates 
+            LOWORD(lParam)-EditRect.left-20,                // width of client area 
+            20,                // height of client area 
+            TRUE);                 // repaint window 
+        return 0;
+    }
+    case WM_DESTROY:
+        PostQuitMessage(0);
+        break;
+    default:
+        return DefWindowProc(hWnd, message, wParam, lParam);
+    }
+    return 0;
+}
+void ShowIPandPORT(HDC hdc, RECT* TextRect) {
+    TextRect->top += 20;
+    TextRect->bottom += 20;
+    DrawText(hdc, WSAnb_Client.IPString, -1, TextRect, DT_VCENTER | DT_LEFT);
+    TextRect->top += 20;
+    TextRect->bottom += 20;
+    DrawText(hdc, WSAnb_Client.PortString, -1, TextRect, DT_VCENTER | DT_LEFT);
+}
+void ShowMessageControls(BOOL show) {
+    ShowWindow(hwndStatic, show? SW_SHOW:SW_HIDE);
+    ShowWindow(hwndStatic2, show ? SW_SHOW : SW_HIDE);
+    ShowWindow(hwndMesageRec, show ? SW_SHOW : SW_HIDE);
+    ShowWindow(hwndEdit, show ? SW_SHOW : SW_HIDE);
+    ShowWindow(hwndButton, show ? SW_SHOW : SW_HIDE);
+}
+// Controlador de mensajes del cuadro Acerca de.
+INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
+{
+    UNREFERENCED_PARAMETER(lParam);
+    switch (message)
+    {
+    case WM_INITDIALOG:
+        return (INT_PTR)TRUE;
+
+    case WM_COMMAND:
+        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+        {
+            EndDialog(hDlg, LOWORD(wParam));
+            return (INT_PTR)TRUE;
+        }
+        break;
+    }
+    return (INT_PTR)FALSE;
+}
+
+void XTrace0(LPCTSTR lpszText)
+{
+    ::OutputDebugString(lpszText);
+}
+
+void XTrace(LPCTSTR lpszFormat, ...)
+{
+    va_list args;
+    va_start(args, lpszFormat);
+    int nBuf;
+    TCHAR szBuffer[512]; // get rid of this hard-coded buffer
+    nBuf = _vstprintf_s(szBuffer, 511, lpszFormat, args);
+    ::OutputDebugString(szBuffer);
+    va_end(args);
+}
---
+++ 15 - WSA non-blocking Client/15 - WSA non-blocking Client.h
@@ -0,0 +1,286 @@
+#pragma once
+
+#include <winsock2.h>
+#include <ws2tcpip.h>
+#include "resource.h"
+
+#pragma comment (lib,"Ws2_32.lib")
+
+#define TIME_OUT_FOR_EVENTS 500
+
+class WSA_non_blocking_Client {
+    //*************** STATUS **********************************************************
+public: enum class STATE { NONE, CONNECTED, LISTENING, REQUESTING };
+public:STATE state = STATE::NONE;
+public:BOOL bConnected = FALSE;
+    //*************** SOCKET *********************************************************
+protected: SOCKET ClientSocket = INVALID_SOCKET;
+public:static const int IPString_Lenght = 50;
+protected: WSADATA wsaData = { 0 };
+protected: sockaddr_in  ClientAdress = { 0 };
+protected: int ClientAdressLen;
+public:      wchar_t IPString[IPString_Lenght] = { 0 };
+public:      wchar_t PortString[IPString_Lenght] = { 0 };
+      //****************** EVENTS *****************************************************
+protected: DWORD EventTotal = 1;
+protected: WSAEVENT EventArray[WSA_MAXIMUM_WAIT_EVENTS];
+         //***************************** VARIOS *****************************************
+public:int TimeOutForEvents = TIME_OUT_FOR_EVENTS;
+public:int lastWSAError = 0;
+protected: int iResult = 0;
+protected: static const int lpBufferWindowsErrorLen = 1000;
+protected: wchar_t lpBufferWindowsError[lpBufferWindowsErrorLen];
+
+/// <summary>
+/// Muestra un mensaje en la ventana del depurador
+/// </summary>
+/// <param name="lpszText">Texto a mostrar</param>
+void XTrace0(LPCTSTR lpszText)
+{
+    ::OutputDebugString(lpszText);
+}
+
+/// <summary>
+/// Muestra un mensaje en la ventana del depurador compatible con la sintaxis printf()
+/// </summary>
+/// <param name="lpszFormat">Texto a mostrar</param>
+/// <param name="">Datos a insertar en el texto</param>
+void XTrace(LPCTSTR lpszFormat, ...)
+{
+    va_list args;
+    va_start(args, lpszFormat);
+    int nBuf;
+    TCHAR szBuffer[512]; // get rid of this hard-coded buffer
+    nBuf = _vstprintf_s(szBuffer, 511, lpszFormat, args);
+    ::OutputDebugString(szBuffer);
+    va_end(args);
+}
+
+/// <summary>
+/// Inicializa la maquinaria WinSock2 de windows.
+/// Inicializa el socket cliente.
+/// Inicializa el evento FD_CONNECT|FD_CLOSE|FD_READ|FD_WRITE.
+/// No altera el STATUS del objeto WSA_non_blocking_Client
+/// </summary>
+/// <returns>TRUE si el socket está inicializado y enlazado con el Evento.
+/// FALSE si algo ha fallado. lastWSAError guarda el ultimo error generado</returns>
+public: int CreateClientSocket() {
+
+    lastWSAError = 0;
+    // socket() data *******************************************************************
+    int iFamily = AF_INET;
+    int iType = SOCK_STREAM;
+    int iProtocol = IPPROTO_TCP;
+ // Initialize Winsock*****************************************************************
+    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
+    if (iResult != 0) {
+        lastWSAError = WSAGetLastError();
+        XTrace(L"WSAStartup failed, returned= %d. lastWSAError=%u\n", iResult, lastWSAError);
+        return FALSE;
+    }
+    // Create socket****************************************************************
+    ClientSocket = socket(iFamily, iType, iProtocol);
+    if (ClientSocket == INVALID_SOCKET)
+    {
+        lastWSAError = WSAGetLastError();
+        XTrace(L"socket function failed with error = %d\n",lastWSAError );
+        return FALSE;
+    }
+    // Associate event types  FD_READ|FD_WRITE| FD_CONNECT |FD_CLOSE*****************************************
+        // with the listening socket and NewEvent
+        // Create new event
+    EventArray[0] = WSACreateEvent();
+    iResult = WSAEventSelect(ClientSocket, EventArray[0], FD_READ|FD_WRITE | FD_CONNECT | FD_CLOSE);
+    if (iResult == SOCKET_ERROR) {
+        lastWSAError = WSAGetLastError();
+        XTrace(L"WSAEventSelect failed with error %u: %s\n", lastWSAError, WindowsErrorToString(lastWSAError));
+        closesocket(ClientSocket);
+        WSACleanup();
+        return FALSE;
+    }
+    return TRUE;
+}
+/// <summary>
+/// Inicia una conexion con el SERVIDOR en la IP y puertos especificados.
+/// Si no consigue conectar, pero la orden de conectar se ejecuta satisfactoriamente,
+/// coloca el STATUS en "REQUESTING" y WSA_non_blocking_Client entra en espera 
+/// del disparo de un evento "FD_CONNECT". El metodo regresa inmediatamente ,
+/// NO bloquea la aplicacion, pero la aplicacion debe
+/// llamar a WSAnb_Client.Attemp_connect() para volver a intentarlo y a 
+/// WSAnb_Client.testForEvents() regularmente ( con un WM_TIMER, por ejemplo)
+/// para reaccionar al evento FD_CONNECT que se producirá cuando el Servidor acepte la 
+/// conexion solicitada.
+/// Attemp_connect() modifica STATUS- CONNECTED si se ha conectado a la primera o NONE
+/// si se ha rechazado por tiempo para no bloquear la aplicacion
+/// </summary>
+/// <param name="IPString">IP v4 del servidor al que se quiere conectar</param>
+/// <param name="port">Puerto de la IP al que se quiere conectar</param>
+/// <returns>TRUE si se ha conectado o si se ha rechazado para no bloquear. FALSE si algo a fallado. 
+/// lastWSAError guarda el último valor de WSAGetLastError. entre los posibles valores de lastWSAError,
+/// WSAEWOULDBLOCK indica que se ha cancelado el intento de conexion para no bloquear </returns>
+public: BOOL Attemp_connect(wchar_t* IPString, int port) {
+    lastWSAError = 0;
+    if (bConnected)return TRUE;
+    IN_ADDR in_addr = { 0 };
+    //Guarda el string IP u el Puerto recibidos
+    //actualiza el estado de la clase
+    wcscpy_s(this->IPString, this->IPString_Lenght, IPString);
+    _itow_s(port, this->PortString, 10);
+    if (InetPton(AF_INET, IPString, &in_addr) != 1) {
+        lastWSAError = WSAGetLastError();
+        XTrace(L"InetPton error %u\n", lastWSAError);
+        closesocket(ClientSocket);
+        WSACloseEvent(EventArray[0]);
+        WSACleanup();
+        return FALSE;
+
+    }
+    ClientAdressLen = sizeof(ClientAdress);
+    ClientAdress.sin_family = AF_INET;
+    ClientAdress.sin_addr = in_addr;
+    ClientAdress.sin_port = htons(port);
+    iResult = connect(ClientSocket, (SOCKADDR*)&ClientAdress, ClientAdressLen);
+    if (iResult == SOCKET_ERROR) {
+        lastWSAError = WSAGetLastError();
+        XTrace(L"connect failed with error %u : %s\n", lastWSAError, WindowsErrorToString(lastWSAError));
+        switch (lastWSAError)
+        {
+        case WSAEWOULDBLOCK:
+            bConnected = FALSE;
+            state = STATE::REQUESTING;
+            return TRUE;
+        default:
+            break;
+        }
+        return FALSE;
+    }
+    bConnected = TRUE;
+    state = STATE::CONNECTED;
+    XTrace(L"Connected to server.\n");
+    return TRUE;
+
+}
+/// <summary>
+/// Evalua los Eventos registrados y reacciona apropiadamente.
+/// FD_CONNECT (conexion rechazada para no bloquear). coloca STATUS en REQUESTING
+/// FD_CONNECT (conexion aceptada por el servidor). coloca STATUS en CONNECTED y bConected=true,
+///permitiendo la transmision de datos.
+/// FD_CLOSE Si la conexion se cierra. Destruye el socket cliente y crea uno nuevo para ponerlo
+/// nuevamente en modo "escucha". Actualiza el STATUS a NONE y bConnected a false;
+/// </summary>
+/// <returns>Devuelve cero si ha funconado, SOCKET_ERROR si ha fallado</returns>
+public:int testForEvents() {
+
+    lastWSAError = 0;
+    WSANETWORKEVENTS NetworkEvents = { 0 };
+    // Wait for one of the sockets to receive I/O notification and
+    DWORD Event = WSAWaitForMultipleEvents(
+        EventTotal,             //The number of event object handles in the array pointed to by lphEvents. 
+        EventArray,             //A pointer to an array of event object handles.              
+        FALSE,                  // If FALSE, the function returns when any of the event objects is signaled.
+        TimeOutForEvents,       //The time-out interval, in milliseconds.
+        FALSE                   //If FALSE, the thread is not placed in an alertable wait state and I/O completion routines are not executed.
+    );
+    switch (Event)
+    {
+    case WSA_WAIT_FAILED:
+    {
+        lastWSAError = WSAGetLastError();
+        XTrace(L"WSAWaitForMultipleEvents() failed with error %u: %s\n", lastWSAError, WindowsErrorToString(lastWSAError));
+        return SOCKET_ERROR;
+    }
+    case WSA_WAIT_IO_COMPLETION:
+        XTrace(L"WSAWaitForMultipleEvents() WSA_WAIT_IO_COMPLETION\n");
+        return 0;
+    case WSA_WAIT_TIMEOUT:
+        XTrace(L"WSAWaitForMultipleEvents() WSA_WAIT_TIMEOUT\n");
+        return 0;
+    default:
+        break;
+    }
+    XTrace(L"WSAWaitForMultipleEvents() is pretty damn OK!\n");
+    iResult = WSAEnumNetworkEvents(
+        ClientSocket,          //A descriptor identifying the socket.
+        EventArray[Event - WSA_WAIT_EVENT_0],           //An optional handle identifying an associated event object to be reset.
+        &NetworkEvents);                                //A structure that is filled with a record of network events that occurred and any associated error codes.
+    if (iResult == SOCKET_ERROR)
+    {
+        lastWSAError = WSAGetLastError();
+        XTrace(L"WSAEnumNetworkEvents() failed with error %u: %s\n", lastWSAError, WindowsErrorToString(lastWSAError));
+        return SOCKET_ERROR;
+    }
+    if (NetworkEvents.lNetworkEvents == 0) {
+        XTrace(L"WSAEnumNetworkEvents() se ha disparado. Pero el evento no está definido.\n");
+           return 0;
+    }
+    if (NetworkEvents.lNetworkEvents & FD_ACCEPT) {
+        XTrace(L"FD_ACCEPT\n");
+        return 0;
+    }
+    if (NetworkEvents.lNetworkEvents & FD_CLOSE) {
+        XTrace(L"FD_CLOSE\n");
+        closesocket(ClientSocket);
+        WSACloseEvent(EventArray[0]);
+        CreateClientSocket();
+        bConnected = FALSE;
+        state = STATE::NONE;
+        return 0;
+    }
+    if (NetworkEvents.lNetworkEvents & FD_CONNECT) {
+        if (NetworkEvents.iErrorCode[FD_CONNECT_BIT] != 0) {
+            XTrace(L"FD_CONNECT conexion rechazada\n");
+            bConnected = FALSE;
+            state = STATE::NONE;
+            return 0;
+        }
+        else
+        {
+            XTrace(L"FD_CONNECT\n");
+            bConnected = TRUE;
+            state = STATE::CONNECTED;
+            return 0;
+        }
+    }
+    if (NetworkEvents.lNetworkEvents & FD_READ) {
+        if (NetworkEvents.iErrorCode[FD_READ_BIT] != 0) {
+            XTrace(L"FD_READ ha devuelto error %u:\n", NetworkEvents.iErrorCode[FD_READ_BIT]);
+            return 0;
+        }
+        else {
+            XTrace(L"FD_READ\n");
+            return 0;
+        }
+    }
+    if (NetworkEvents.lNetworkEvents & FD_WRITE) {
+        if (NetworkEvents.iErrorCode[FD_WRITE_BIT] != 0) {
+            XTrace(L"FD_WRITE ha devuelto error %u:\n", NetworkEvents.iErrorCode[FD_WRITE_BIT]);
+            return 0;
+        }
+        else {
+            XTrace(L"FD_WRITE\n");
+            return 0;
+        }
+    }
+    return 0;
+}
+/// <summary>
+/// Devuelve un puntero a una cadena con el Error de Windos traducido para ser leido por un humano
+/// </summary>
+/// <param name="ErrorCode">Codigo de error de windows</param>
+/// <returns>Cadena con el texto en humano</returns>
+public: wchar_t* WindowsErrorToString(int ErrorCode)
+{
+
+    if (FormatMessageW(FORMAT_MESSAGE_FROM_SYSTEM,
+        NULL,
+        ErrorCode,
+        0,
+        lpBufferWindowsError,
+        lpBufferWindowsErrorLen,
+        NULL) == 0)
+    {
+        XTrace(L"Error with FormatMessage\n");
+    }
+    return lpBufferWindowsError;
+}
+};
